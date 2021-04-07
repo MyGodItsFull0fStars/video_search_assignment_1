b327541d58d4806ce7c54bb97c4d86b0ae7885bd
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-04-07 21:53:18 +0200

    Now the detected key frames get written into a separate folder when executing the main function in main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import shot_detection
 import cv2
+import matplotlib.pyplot as plot
+import utils
+import numpy as np
 
 if __name__ == '__main__':
     print('start')
@@ -7,5 +10,11 @@ if __name__ == '__main__':
     st.capture_video('everest.mp4')
 
     print(len(st.detected_shots))
+
+    # utils.plot_image_list(st.detected_shots)
+
+    utils.write_images_to_disk(st.detected_shots)
+    # plot.imshow(cv2.cvtColor(st.detected_shots[0], cv2.COLOR_BGR2RGB))
+    # plot.show()
 
     print('end')
